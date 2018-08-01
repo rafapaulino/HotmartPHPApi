3996f5e55d5cb18e88050deb511c2632f72b5cfa
Rafael Brigagao Paulino <rafael.paulino@FIESP.COM.BR>
2018-08-01 17:33:36 -0300

    Iniciando a classe de conexão

---
+++ HotmartRequest.php
@@ -0,0 +1,36 @@
+<?php 
+
+class HotmartRequest
+{
+    private $_ch;
+
+    public function __construct()
+    {
+        $this->_ch = curl_init();
+    }
+
+    public function __destruct()
+    {
+        curl_close($this->_ch);
+    }
+
+    public function send($url, $data)
+    {
+        try {
+            $fields_string = "";
+            //url-ify the data for the POST
+            foreach($data as $key => $value) { $fields_string .= $key . '=' . $value . '&'; }
+            rtrim($fields_string, '&');
+    
+            //set the url, number of POST vars, POST data
+            curl_setopt($this->_ch, CURLOPT_URL, $url);
+            curl_setopt($this->_ch, CURLOPT_POST, count($data));
+            curl_setopt($this->_ch, CURLOPT_POSTFIELDS, $fields_string);
+    
+            return json_decode(curl_exec($this->_ch), true);
+
+        } catch(\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
